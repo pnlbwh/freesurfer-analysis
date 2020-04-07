74a0ac0c759c82d1945aa3a5a9d6c60c4d0a6114
Tashrif Billah <tashrifbillah@gmail.com>
2020-04-07 11:09:48 -0400

    interactive table * outliers colored in red * tooltip added for each cell * control given for nilearn snapshot or freeview * striped rows, deletable columns

---
+++ scripts/summary-table.py
@@ -0,0 +1,79 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import dash_table
+import pandas as pd
+
+df = pd.read_csv('C://Users/tashr/Documents/fs-stats/outliers.csv')
+
+app = dash.Dash(__name__)
+
+data_condition=[{
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(240, 240, 240)'
+        }]
+
+for d in [{
+            'if': {
+            'column_id': c,
+            'filter_query': f'{{{c}}} eq x',
+            },
+            'backgroundColor': 'red',
+            'color': 'black',
+            'fontWeight': 'bold'
+        } for c in df.columns]:
+    data_condition.append(d)
+
+
+app.layout = html.Div([
+
+        'Type of visual inspection upon selecting a cell: ',
+        html.Div([
+            dcc.Dropdown(
+                id='view-type',
+                options=[{'label': i, 'value': i} for i in ['snapshot','freeview']],
+                value='snapshot'
+            )
+        ],
+        style = {'width': '10%',}), # 'display': 'inline-block'}),
+        html.Br(),
+
+        dash_table.DataTable(
+        id='table',
+        columns=[{'name': i, 'id': i, 'deletable': True} for i in df.columns],
+        data=df.to_dict('records'),
+        filter_action='native',
+        style_data_conditional= data_condition,
+
+        style_header={
+            'backgroundColor': 'rgb(230, 230, 230)',
+            'fontWeight': 'bold'
+        },
+
+        tooltip_data= [{c:
+                {
+                    'type': 'text',
+                    'value': f'{r},{c}'
+                } for c in df.columns
+        } for r in df[df.columns[0]].values]
+    ),
+    html.Div(id='table-tooltip')
+])
+
+
+@app.callback(Output('table-tooltip', 'children'),
+              [Input('table', 'selected_cells'),
+               Input('view-type', 'value')])
+def get_active_cell(selected_cells, view_type):
+    if selected_cells:
+        temp= selected_cells[0]
+        row= temp['row']
+        col= temp['column']
+        print(row, col)
+
+    print(view_type)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port= 8040, host= 'localhost')
